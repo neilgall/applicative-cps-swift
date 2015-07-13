bc29fa49e07bd388692cc60a3cd8c435a68ba483
Neil Gall <neil.gall@bitwink.com>
2015-07-13 19:18:08 +0100

    Further cleanup for brevity

--- Applicatives.playground/Contents.swift
+++ Applicatives.playground/Contents.swift
@@ -8,24 +8,12 @@ public func pure<A,B>(f: A -> B) -> ((A -> B) -> ()) -> () {
 
 infix operator <%> { associativity left }
 public func <%> <A,B>(f: A -> B, fa: (A -> ()) -> ()) -> (B -> ()) -> () {
-    return { completion in
-        fa { a in
-            let b = f(a)
-            completion(b)
-        }
-    }
+    return { cont in fa { cont(f($0)) } }
 }
 
 infix operator <*> { associativity left }
 public func <*> <A,B>(ff: ((A -> B) -> ()) -> (), fa: (A -> ()) -> ()) -> (B -> ()) -> () {
-    return { completion in
-        fa { a in
-            ff { f in
-                let b = f(a)
-                completion(b)
-            }
-        }
-    }
+    return { cont in fa { a in ff { cont($0(a)) } } }
 }
 
 func calc(i: Int)(j: Int)(k: Int) -> Int {
@@ -33,7 +21,7 @@ func calc(i: Int)(j: Int)(k: Int) -> Int {
 }
 
 func async<T>(t: T)(c: T -> ()) {
-    if (async) {
+    if (runAsync) {
         dispatch_async(dispatch_get_main_queue()) {
             c(t)
         }
@@ -42,7 +30,7 @@ func async<T>(t: T)(c: T -> ()) {
     }
 }
 
-let async = false
+let runAsync = false
 
 let x = calc(2) <%> async(5) <*> async(3)
 x { print("x=\($0)") }
