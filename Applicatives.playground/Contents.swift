4e40f26df59121d987cfba08079aaa506f688587
Neil Gall <neil.gall@bitwink.com>
2015-07-13 18:57:49 +0100

    Remove brain scaffold

--- Applicatives.playground/Contents.swift
+++ Applicatives.playground/Contents.swift
@@ -1,57 +1,52 @@
 
 import Foundation
 
-public struct Cont<A> {
-    let run : (A -> ()) -> ()
-}
 
 public func pure<A,B>(f: A -> B) -> ((A -> B) -> ()) -> () {
-    return Cont(run: { _ in f }).run
-}
-
-public func fmap<A,B> (f: A -> B, fa: Cont<A>) -> Cont<B> {
-    return Cont(run: { completion in
-        fa.run { a in
-            let b = f(a)
-            completion(b)
-        }
-    })
-}
-
-public func apply<A,B> (ff: Cont<A -> B>, fa: Cont<A>) -> Cont<B> {
-    return Cont(run: { completion in
-        fa.run { a in
-            ff.run { f in
-                let b = f(a)
-                completion(b)
-            }
-        }
-    })
+    return { $0(f) }
 }
 
 infix operator <%> { associativity left }
 public func <%> <A,B>(f: A -> B, fa: (A -> ()) -> ()) -> (B -> ()) -> () {
-    return fmap(f, fa: Cont(run: fa)).run
+    return { completion in
+        fa { a in
+            let b = f(a)
+            completion(b)
+        }
+    }
 }
 
 infix operator <*> { associativity left }
-public func <*> <A,B>(f: ((A -> B) -> ()) -> (), fa: (A -> ()) -> ()) -> (B -> ()) -> () {
-    return apply(Cont(run: f), fa: Cont(run: fa)).run
+public func <*> <A,B>(ff: ((A -> B) -> ()) -> (), fa: (A -> ()) -> ()) -> (B -> ()) -> () {
+    return { completion in
+        fa { a in
+            ff { f in
+                let b = f(a)
+                completion(b)
+            }
+        }
+    }
 }
-
 
 func calc(i: Int)(j: Int)(k: Int) -> Int {
     return (i + j) * k
 }
 
 func async<T>(t: T)(c: T -> ()) {
-    dispatch_async(dispatch_get_main_queue()) {
+    if (async) {
+        dispatch_async(dispatch_get_main_queue()) {
+            c(t)
+        }
+    } else {
         c(t)
     }
 }
 
-let x = calc(2) <%> async(5) <*> async(3)
-x { print($0) }
+let async = false
 
-let y = pure(calc) <*> async(2) <*> async(3) <*> async(1)
-y { print($0) }
+let x = calc(2) <%> async(5) <*> async(3)
+x { print("x=\($0)") }
+
+let y = pure(calc) <*> async(2) <*> async(3) <*> async(6)
+y { print("y=\($0)") }
+
